af95c28dcccc74ebf3628f29de19b2eb9240827a
Bhaskar-Majumdar <bhaskar15-9988@diu.edu.bd>
2025-04-26 23:25:10 +0600

    table data find

--- tests/playwrightLocator.spec.js
+++ tests/playwrightLocator.spec.js
@@ -13,10 +13,10 @@ test( 'Playwright Specific Locator' , async ( {page} ) => {
     let imgLocator2 = page.getByTitle("naveenopencart");
     let password = page.getByPlaceholder("Password");
 
-    await firstNameTxt.fill("Salakn");
-    await lastNameTxt.fill("Khn");
+    await firstNameTxt.fill("Salaskn");
+    await lastNameTxt.fill("Khnaa");
     await personal.isVisible();
-    await email.fill("abwsgdfs@gmail.com");
+    await email.fill("abwsdgdfs@gmail.com");
     await phoneNum.fill("123282318");
     await password.first().fill("1234");
     await password.last().fill("1234")
